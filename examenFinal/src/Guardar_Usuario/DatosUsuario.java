3e258bcddcca14ea6ab55b880704431837bcc010
tonyCR31 <tonyfacio31@gmail.com>
2021-08-25 22:06:13 -0600

    Examen

---
+++ examenFinal/src/Guardar_Usuario/DatosUsuario.java
@@ -0,0 +1,44 @@
+package Guardar_Usuario;
+
+
+
+public class DatosUsuario {
+
+    private String nombre;
+    private String apellido;
+    private String usuario;
+    private String contraseña;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+}
---
+++ examenFinal/src/Guardar_Usuario/IniciarComponentes.java
@@ -0,0 +1,9 @@
+
+package Guardar_Usuario;
+
+import java.util.ArrayList;
+
+public class IniciarComponentes {
+    public static ArrayList<DatosUsuario> usuarios = new ArrayList<DatosUsuario>();
+    
+}
